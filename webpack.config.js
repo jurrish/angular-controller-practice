1032420376c04c4e06e59ac18197b70440c06708
Frank Iriarte <jurrish@gmail.com>
2017-05-23 10:57:51 -0700

    working on controller syntax

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const HTMLPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  entry: `${__dirname}/app/entry.js`,
+  plugins: [
+    new HTMLPlugin({
+      template: `${__dirname}/app/index.js`
+    }),
+    new ExtractTextPlugin('bundle.css'),
+  ],
+  output: {
+    path: `${__dirname}/build`,
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract(['css-loader', 'sass-loader']),
+      },
+      {
+        test: /\.html$/,
+        loader: 'html-loader',
+      },
+    ],
+  }
+};
